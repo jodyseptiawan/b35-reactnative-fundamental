ccbbaac24c3d099d2c6e2c9eb7fad053e687e738
Jedeye <jody.septiawan5@gmail.com>
2022-06-29 09:44:48 +0700

    update

--- src/screens/embedExpression.js
+++ src/screens/embedExpression.js
@@ -4,13 +4,17 @@ import { View } from "react-native";
 
 export default function EmbedExpression() {
   // Create Function Here
+  function getMajor() {
+    return "Full-Stack"
+  }
 
   // Create a variable Here
+  const companyName = 'Dumbways Indonesia'
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      Welcome To {companyName} Class {getMajor()}
     </View>
   );
 }
