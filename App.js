360bf1b8cbfca5be01bb2e1fbaa034ed5b3f2458
Jedeye <jody.septiawan5@gmail.com>
2022-06-29 11:44:51 +0700

    update

--- App.js
+++ App.js
@@ -3,18 +3,65 @@ import React from "react";
 import { NativeBaseProvider, extendTheme } from "native-base";
 
 // Import font with Expo
+import AppLoading from 'expo-app-loading';
+import { useFonts, BalsamiqSans_400Regular, BalsamiqSans_400Regular_Italic } from '@expo-google-fonts/balsamiq-sans'
 
 // Import Container
+import Container from './Container'
 
 export default function App() {
   // Load Font with Expo
+  let [fontsLoaded] = useFonts({
+    BalsamiqSans_400Regular,
+    BalsamiqSans_400Regular_Italic
+  })
 
   // Setup Font
+  const fontConfig = {
+    BalsamiqSans: {
+      400: {
+        normal: 'BalsamiqSans_400Regular',
+        italic: 'BalsamiqSans_400Regular_Italic'
+      }
+    }
+  }
 
   // Setup Theme
+  const customeColor = {
+    primary: {
+      50: "#E3F2F9",
+      100: "#C5E4F3",
+      200: "#A2D4EC",
+      300: "#7AC1E4",
+      400: "#47A9DA",
+      500: "#0088CC",
+      600: "#007AB8",
+      700: "#006BA1",
+      800: "#005885",
+      900: "#003F5E",
+    },
+  }
 
   // Configuration Native Base Custom Theme
-  return (
-    // Setup Provider
-  );
+
+  const theme = extendTheme({
+    colors: customeColor,
+    fontConfig,
+    fonts: {
+      header: 'BalsamiqSans',
+      body: 'BalsamiqSans',
+      mono: 'BalsamiqSans'
+    },
+  })  
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <NativeBaseProvider theme={theme}>
+        <Container />
+      </NativeBaseProvider>
+    );
+  }
+
 }
--- src/screens/formNativeBase.js
+++ src/screens/formNativeBase.js
@@ -18,5 +18,16 @@ import {
 } from "native-base";
 
 export default function FormNativeBase() {
-  return <Box>{/* Code Here */}</Box>;
+  return <Box bg="primary.100" p={10} flex={1} justifyContent="center">
+    <Heading size="md" textAlign="center" mb={10}>Sign In</Heading>
+    <FormControl mb={5}>
+      <FormControl.Label _text={{color: 'primary.800'}}>Email</FormControl.Label>
+      <Input borderColor="primary.500" />
+    </FormControl>
+    <FormControl>
+      <FormControl.Label _text={{color: 'primary.800'}}>Password</FormControl.Label>
+      <Input borderColor="primary.500" />
+    </FormControl>
+    <Button mt={5}>Sign In</Button>
+  </Box>;
 }
