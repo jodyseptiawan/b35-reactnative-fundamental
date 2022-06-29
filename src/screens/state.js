ba5fb3ae77259cb131d0f8fab5d0bd2b0e29b747
Jedeye <jody.septiawan5@gmail.com>
2022-06-29 10:06:31 +0700

    update

--- src/screens/state.js
+++ src/screens/state.js
@@ -1,18 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import React, {useState} from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function State() {
   // Init State
+  const [counter, setCounter] = useState(0)
 
   //Create Function Add
+  const Add = () => {
+    setCounter(counter + 1)
+  }
 
   //Create Function Less
+  const Less = () => {
+    setCounter(counter - 1)
+  }
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      
+      <Text>{counter}</Text>
+
+      <TouchableOpacity onPress={Add}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={Less}>
+        <Text>Less</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
