14876c6ae94ab637396eb239095838a297f59f8c
Jedeye <jody.septiawan5@gmail.com>
2022-06-29 10:40:28 +0700

    update

--- src/screens/flatList.js
+++ src/screens/flatList.js
@@ -1,13 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, FlatList, Text } from "react-native";
 
 export default function List() {
   // Make Dummy Data with Array
+  const peoples = [
+    {
+      id: 1,
+      name: 'Ilham'
+    },
+    {
+      id: 2,
+      name: 'Alvin'
+    },
+    {
+      id: 3,
+      name: 'Fadhil'
+    }
+  ]
+
+
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <FlatList 
+        data={peoples}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+        keyExtractor={(item) => item.id.toString()}
+        refreshing={true} />
     </View>
   );
 }
