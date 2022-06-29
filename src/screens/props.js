be181869d7527de733041504c141f5c7bb9114ff
Jedeye <jody.septiawan5@gmail.com>
2022-06-29 09:58:22 +0700

    udpate

--- src/screens/props.js
+++ src/screens/props.js
@@ -1,14 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
+
+import List from "../components/list";
 
 export default function Props() {
   // Create variable to insert link Image
+  let pic = {
+    uri: 'https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/homepage/families-gallery/2022/04_12/family_chooser_tecnica_m.png'
+  }
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+
+      <Image 
+        source={pic}
+        style={{width: '100%', height: 200}}
+      />
+
+      <List title="BMW" />
+      <List title="Mercedes-Benz" />
+      <List title="Ducati" />
     </View>
   );
 }
