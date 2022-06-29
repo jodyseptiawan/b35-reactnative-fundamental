2467a965f07f718e604efb2884b2a2926aa2452c
Jedeye <jody.septiawan5@gmail.com>
2022-06-29 10:30:35 +0700

    update

--- src/screens/map.js
+++ src/screens/map.js
@@ -1,14 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function Map() {
   // Create Dummy Data With Array
+  const cars = ['BMW', 'Ducati', 'Ferrari', 'Tesla']
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <Text>This is List with MAP</Text>
+      {cars.map((car, idx) => (
+        <Text key={idx}>{car}</Text>
+      ))}
     </View>
   );
 }
